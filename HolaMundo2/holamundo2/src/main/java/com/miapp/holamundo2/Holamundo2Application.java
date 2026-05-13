d508110c80ab4d877b7c3d452cc04188bf5d6cca
matelat-daw <cesarmatelat@gmail.com>
2026-05-13 19:35:33 +0100

    Update HolaMundo2

---
+++ HolaMundo2/holamundo2/src/main/java/com/miapp/holamundo2/Holamundo2Application.java
@@ -0,0 +1,12 @@
+package com.miapp.holamundo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Holamundo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Holamundo2Application.class, args);
+	}
+}
---
+++ HolaMundo2/holamundo2/src/test/java/com/miapp/holamundo2/Holamundo2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.miapp.holamundo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Holamundo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
